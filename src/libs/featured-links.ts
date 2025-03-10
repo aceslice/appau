988007aa072eaf6e7f723456c9e12ecffd1fa3a9
Gideon Appau <gideonad.codez@gmail.com>
2025-03-10 23:20:11 +0000

    minore ui tweak

---
+++ src/libs/featured-links.ts
@@ -0,0 +1,26 @@
+
+
+const featuredLinks = [
+	{
+		title: "Mangobase 🥭",
+		description:
+			"Low-code Javascript backend framework for Node and Bun runtimes.",
+		link: "degreat.co.uk/mangobase",
+	},
+	{
+		title: "Adeton",
+		description:
+			"This is a SaaS project I founded; an e-commerce enabler. Think Shopify but tailored for the Ghanaian merchant.",
+		link: "adeton.io",
+	},
+	{
+		title: "Devlog",
+		description:
+			"This is a Youtube channel I share progress on stuff I'm working on. You'll also find some tutorials on code, 3d, etc. It's fun, check it out!",
+		link: "youtube.com/@notgr",
+		footer: "Please subscribe",
+		icon: "i-lucide-youtube text-red-500 text-lg",
+	},
+];
+
+export default featuredLinks;
